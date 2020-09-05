fe968825d7c14eedb49c4824ea28667158dffe5e
Alejandro González Reyes <alexglzrys@gmail.com>
2020-09-05 17:00:37 -0500

    Asociar, configurar e inicializar el plugin de DataTable en el proyecto

--- src/app.js
+++ src/app.js
@@ -1,3 +1,49 @@
 $(document).ready(function() {
-    $("#countries").DataTable()
+
+    $("#countries").DataTable({
+        "language": {
+            "decimal":        "",
+            "emptyTable":     "No hay datos disponibles en la tabla",
+            "info":           "Mostrando _START_ a _END_ de _TOTAL_ registros",
+            "infoEmpty":      "Mostrando 0 a 0 de 0 registros",
+            "infoFiltered":   "(Filtrado de _MAX_ registros totales)",
+            "infoPostFix":    "",
+            "thousands":      ",",
+            "lengthMenu":     "Mostrar _MENU_ registros",
+            "loadingRecords": "Cargando...",
+            "processing":     "Procesando...",
+            "search":         "Buscar:",
+            "zeroRecords":    "No se encontraron registros que coincidan con los términos de búsqueda",
+            "paginate": {
+                "first":      "Primero",
+                "last":       "Ultimo",
+                "next":       "Siguiente",
+                "previous":   "Anterior"
+            },
+            "aria": {
+                "sortAscending":  ": activar para ordenar la columna de forma ascendente",
+                "sortDescending": ": activar para ordenar la columna de forma descendente"
+            }
+        },
+        "processing": true,
+        "deferRender": true,
+        "ajax": {
+            "url": "https://restcountries.eu/rest/v2/all",
+            "type": "GET",
+            "dataSrc": "",
+        },
+        "columns": [
+            { "data": "name" },
+            { "data": "region" },
+            { "data": "population" },
+            {
+                "data": "flag",
+                "render": function(data, type, row, meta) {
+                    return '<img src="'+data+'" />'
+                }
+            }
+        ]
+    })
+
 })
+
